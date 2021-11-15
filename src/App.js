2762f111116d7d56cebf7a96e88bcbcb3f6154b3
Joel <jp@follett.com>
2021-11-15 17:33:26 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import "./App.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor(props) {
+    super(props)
+  
+    this.state = {response:[]
+       
+    }
+  }
+  getData=async()=>
+{
+ const response = await fetch ("https://api.github.com/users")
+ const convertData = await response.json()
+ console.log(convertData)
+this.setState({response:convertData})
 }
+  
+  render() {
+    return (
+      <div>
+        <button onClick={this.getData}>Get data</button>
+        {this.state.response.map((details)=>
+        {
+          return(
+            <div>
+              <h1>{details.login}</h1>
+          <img src={details.avatar_url}/>
+          </div>
 
-export default App;
+          )
+          
+        }
+        )}
+      </div>
+    )
+  }
+}
